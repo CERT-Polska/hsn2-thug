cb9ee26de210b627c7ca32a919e915c3ebe72996
Paweł Chojnowski <pawel.chojnowski@live.com>
2015-07-01 14:11:49 +0200

    autopep8

--- hsn2_thug/hsn2thuganalysisparser.py
+++ hsn2_thug/hsn2thuganalysisparser.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python -tt
 
 # Copyright (c) NASK
-# 
+#
 # This file is part of HoneySpider Network 2.0.
-# 
+#
 # This is a free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -19,66 +19,67 @@
 
 import xml.sax
 
+
 class ThugAnalysisParser(xml.sax.handler.ContentHandler):
-	parser = None
-	behaviour = None
-	behaviours = None
-	inBehaviour = None
-	inBehaviourText = None
-	jsContext = None
-	jsContexts = None
-	inCodeSnippet = None
-	inCodeSegment = None
-	inCodeSegment = None
+    parser = None
+    behaviour = None
+    behaviours = None
+    inBehaviour = None
+    inBehaviourText = None
+    jsContext = None
+    jsContexts = None
+    inCodeSnippet = None
+    inCodeSegment = None
+    inCodeSegment = None
 
-	def __init__(self):
-		self.parser = xml.sax.make_parser()
-		self.parser.setContentHandler(self)
+    def __init__(self):
+        self.parser = xml.sax.make_parser()
+        self.parser.setContentHandler(self)
 
-	def parseFile(self, filePath, saveJSContexts = True):
-		self.behaviours = []
-		self.jsContexts = []
-		self.saveJSContexts = saveJSContexts
-		self.inCodeSnippet = False
-		self.inCodeSegment = False
-		self.inBehaviour = False
-		self.inBehaviourText = False
-		try:
-			self.parser.parse(filePath)
-		except xml.sax.SAXException:
-			return False
-		return (self.behaviours, self.jsContexts)
+    def parseFile(self, filePath, saveJSContexts=True):
+        self.behaviours = []
+        self.jsContexts = []
+        self.saveJSContexts = saveJSContexts
+        self.inCodeSnippet = False
+        self.inCodeSegment = False
+        self.inBehaviour = False
+        self.inBehaviourText = False
+        try:
+            self.parser.parse(filePath)
+        except xml.sax.SAXException:
+            return False
+        return (self.behaviours, self.jsContexts)
 
-	# 3 handler functions
-	def startElement(self, name, attrs):
-		if self.saveJSContexts and name == "Code_Snippet" and attrs.getValueByQName("language") == "Javascript":
-			self.inCodeSnippet = True
-		if name == "Code_Segment" and self.inCodeSnippet:
-			self.jsContext = ""
-			self.inCodeSegment = True
-		if name == "Behavior":
-			self.behaviour = {}
-			self.inBehaviour = True
-		if self.inBehaviour and name == "Text":
-			self.inBehaviourText = True
-		if self.inBehaviour and name == "Discovery_Method":
-			self.behaviour["discovery_method"] = attrs.getValueByQName("method")
+    # 3 handler functions
+    def startElement(self, name, attrs):
+        if self.saveJSContexts and name == "Code_Snippet" and attrs.getValueByQName("language") == "Javascript":
+            self.inCodeSnippet = True
+        if name == "Code_Segment" and self.inCodeSnippet:
+            self.jsContext = ""
+            self.inCodeSegment = True
+        if name == "Behavior":
+            self.behaviour = {}
+            self.inBehaviour = True
+        if self.inBehaviour and name == "Text":
+            self.inBehaviourText = True
+        if self.inBehaviour and name == "Discovery_Method":
+            self.behaviour["discovery_method"] = attrs.getValueByQName("method")
 
-	def endElement(self, name):
-		if name == "Code_Snippet":
-			if self.inCodeSegment:
-				self.jsContexts.append({"id" : len(self.jsContexts)+1, "source" : self.jsContext, "eval" : False})
-				self.jsContext = None
-			self.inCodeSnippet = False
-			self.inCodeSegment = False
-		if name == "Behavior":
-			self.inBehaviour = False
-			self.inBehaviourText = False
-			self.behaviours.append(self.behaviour)
-			self.behaviour = None
+    def endElement(self, name):
+        if name == "Code_Snippet":
+            if self.inCodeSegment:
+                self.jsContexts.append({"id": len(self.jsContexts) + 1, "source": self.jsContext, "eval": False})
+                self.jsContext = None
+            self.inCodeSnippet = False
+            self.inCodeSegment = False
+        if name == "Behavior":
+            self.inBehaviour = False
+            self.inBehaviourText = False
+            self.behaviours.append(self.behaviour)
+            self.behaviour = None
 
-	def characters(self, data):
-		if self.inCodeSegment:
-			self.jsContext = self.jsContext + data
-		if self.inBehaviourText:
-			self.behaviour["description_text"] = self.behaviour.get("description_text","") + data
+    def characters(self, data):
+        if self.inCodeSegment:
+            self.jsContext = self.jsContext + data
+        if self.inBehaviourText:
+            self.behaviour["description_text"] = self.behaviour.get("description_text", "") + data
--- hsn2_thug/hsn2thugservice.py
+++ hsn2_thug/hsn2thugservice.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python -tt
 
 # Copyright (c) NASK
-# 
+#
 # This file is part of HoneySpider Network 2.0.
-# 
+#
 # This is a free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -17,8 +17,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import logging
 from os import path
+import logging
 
 from hsn2_commons.hsn2service import HSN2Service
 from hsn2_commons.hsn2service import startService
@@ -26,23 +26,23 @@ from hsn2_thug.hsn2thugtaskprocessor import ThugTaskProcessor
 
 
 class ThugService(HSN2Service):
-	'''
-	This is the HSN2 service which utilizes the Thug Python low-interaction honeyclient.
-	'''
-	serviceName = "thug"
-	description = "HSN 2 Thug Service"
+    '''
+    This is the HSN2 service which utilizes the Thug Python low-interaction honeyclient.
+    '''
+    serviceName = "thug"
+    description = "HSN 2 Thug Service"
 
-	def extraOptions(self,parser):
-		'''Arguments specific to this service. Receives a parser with the standard options. Returns a modified parser.'''
-		parser.add_argument('--thug', '-t', action='store', help='path to the thug.py file', default="/opt/hsn2/thug/src/thug.py", required=False, dest='thug')
-		return parser
+    def extraOptions(self, parser):
+        '''Arguments specific to this service. Receives a parser with the standard options. Returns a modified parser.'''
+        parser.add_argument('--thug', '-t', action='store', help='path to the thug.py file', default="/opt/hsn2/thug/src/thug.py", required=False, dest='thug')
+        return parser
 
-	def sanityChecks(self, cliargs):
-		passed = HSN2Service.sanityChecks(self, cliargs)
-		if not path.isfile(cliargs.thug):
-			logging.error("'%s' is not a file" % cliargs.thug)
-			passed = False
-		return passed
+    def sanityChecks(self, cliargs):
+        passed = HSN2Service.sanityChecks(self, cliargs)
+        if not path.isfile(cliargs.thug):
+            logging.error("'%s' is not a file" % cliargs.thug)
+            passed = False
+        return passed
 
 if __name__ == '__main__':
-	startService(ThugService,ThugTaskProcessor)
+    startService(ThugService, ThugTaskProcessor)
